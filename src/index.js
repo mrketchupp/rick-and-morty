9de3f6673d10ee5dff6086fadd7f731a874e497b
Kevin Santiago <sonicthe1111@gmail.com>
2023-02-25 03:25:56 -0600

    Primera clase - Creando nodos

--- src/index.js
+++ src/index.js
@@ -3,4 +3,31 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const url = "https://platzi-avo.vercel.app/api/avo";
+
+//web api
+
+async function fetchData(urlAPI) {
+    const response = await fetch(urlAPI);
+    const datos = await response.json();
+    console.log(datos);
+    const todosLosItems = [];
+    datos.data.forEach(item => {
+        // crear imagen
+        const imagen = document.createElement('img');
+
+        // crear titulo
+        const title = document.createElement('h2');
+
+        // crear precio
+        const price = document.createElement('div');
+
+        const container = document.createElement('div')
+        container.append(imagen, title, price);
+
+        todosLosItems.push(container);
+    });
+    document.body.append(...todosLosItems);
+}
+
+fetchData(url)
